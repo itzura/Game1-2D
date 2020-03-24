2c43f7aaff4472f6383b56c12a8a370392695e1b
Rohan Goyal <rohangoyal369@gmail.com>
2020-03-25 01:02:18 +0530

    version 3.0
    Added start path

--- mob.gd
+++ mob.gd
@@ -8,9 +8,6 @@ func _ready():
 	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
 	
 
-func _on_visibility_screen_exited():
-	pass # Replace with function body.
-
 func _on_Visibility_screen_exited():
 	queue_free()
 	
