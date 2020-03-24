79d7a5d34e78b8d24edd3cf0b0d235dc6a6866de
Rohan Goyal <rohangoyal369@gmail.com>
2020-03-24 23:28:38 +0530

    version 1.0
    added mob.gd file
    added main node
    added main.gd file

--- mob.gd
+++ mob.gd
@@ -1,16 +1,16 @@
 extends RigidBody2D
 
+export var min_speed = 150 # minimum speed range
+export var max_speed = 250 #maximum speed range
+var mob_types = ["walk","swim","Fly"]
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+	
+
+func _on_visibility_screen_exited():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Visibility_screen_exited():
+	queue_free()
+	
